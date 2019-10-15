dd4541cb344221acabaa2e2cd0b0b1893631a43a
Max Tang <tw7613781@gmail.com>
2019-10-16 01:23:38 +0900

    feat: add sqliteDB

---
+++ sqliteDB.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const sqlite3 = require('sqlite3').verbose();
+const log4js = require('./utils');
+
+const logger = log4js.Logger('sqliteDB')
+
+class SQLiteDB {
+    constructor(tableName) {
+        this.db = new sqlite3.Database('./data/data.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE), (e) => {
+            if (e) {
+                logger.error(`init database error: ${e}`);
+            }
+            logger.info('connected to the data db');
+        }
+
+        this.table = tableName
+
+        const tableSchema = `CREATE TABLE IF NOT EXISTS ${this.table} (
+            hash text NOT NULL PRIMARY KEY,
+            amount text,
+            difficulty text,
+            fee text,
+            length text,
+            volume text,
+            stateRoot text,
+            merkleRoot text,
+            txs text,
+            height, text,
+            timeStamp text,
+            prevBlock text,
+            nonce text,
+            miner text,
+            resultHash
+        )`
+
+        this.db.exec(tableSchema, (e) => {
+            if (e) {
+                logger.error(`init table error: ${e}`);
+            }
+            logger.info(`went to table ${this.table}`);
+        });
+    }
+
+    insert(block) {
+        return new Promise((resolve, reject) => {
+            const sql = `INSERT INTO ${this.table} (
+                hash, amount, difficulty, fee, length, volume,
+                stateRoot, merkleRoot, txs, height, timeStamp,
+                prevBlock, nonce, miner, resultHash
+            ) VALUES ( ${block.hash}, ${block.amount}, ${block.difficulty},
+                ${block.fee}, ${block.length}, ${block.volume},
+                ${block.stateRoot}, ${block.merkleRoot}, ${block.txs},
+                ${block.height}, ${block.timeStamp}, ${block.prevBlock},
+                ${block.monce}, ${block.miner}, ${block.resultHash}
+            );`
+            this.db.run(sql, (e)=> {
+                if (e) {
+                    logger.error(`insert table ${this.table} error`);
+                    reject(e);
+                }
+                resolve();
+            });
+        })
+    }
+
+    closeDB () {
+        return new Promise((resolve, reject) => {
+            this.db.close((e) => {
+                logger.error(`close db error: ${e}`);
+                reject(e);
+            })
+            resolve();
+        })
+    }
+}
+
+module.exports = SQLiteDB
