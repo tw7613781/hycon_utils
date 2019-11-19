3bc61f4abaecaf9717a1d9c5b4e41cc242a1ba2e
Max Tang <tw7613781@gmail.com>
2019-11-19 11:54:31 +0900

    feat: uing hyconjs-util to send tx

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,3 +1,103 @@
+import * as util from "@glosfer/hyconjs-util"
+import { log4js } from "./utils"
+import { MIN_VALUE } from "long"
+
+const request = require("request")
+const config = require("../config")
+
+const logger = log4js.getLogger(__filename)
+const mnemonic = config.mnemonic
+const password = ''
+const host = 'https://network.hycon.io/api/v3'
 
 
-console.log('test')
+interface IAddrInfo {
+    balance: string;
+    nonce: number;
+    address: string;
+}
+
+interface ITxRet {
+    txhash: string
+}
+
+function getAddressInfo(address):Promise<IAddrInfo> {
+    return new Promise( (resolve, reject) => {
+        const endpoint = `/address/${address}`
+        const url = host + endpoint
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(JSON.parse(body))
+            }
+        })
+    })
+}
+
+function outgoingTx(tx): Promise<ITxRet> {
+    return new Promise( (resolve, reject) => {
+        const endpoint = `/tx`
+        const url = host + endpoint
+        const options = {
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            method: "POST",
+            body: JSON.stringify(tx),
+            url,
+        }
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(JSON.parse(body))
+            }
+        })
+    })
+}
+
+
+async function main() {
+    try {
+        const wallet = util.createWallet(mnemonic, password)
+        logger.debug('My Hycon address and private key')
+        logger.debug(wallet.address, wallet.privateKey)
+        logger.debug('--------------------------------')
+        const info: IAddrInfo = await getAddressInfo(wallet.address)
+        logger.debug(info)
+        const nonce = info.nonce + 1
+        const toAddr = 'H3ADKxdfQgHhiowuEyj2NkJF86XqT9vx3'
+        const amount = '10'
+        const fee = '0.000000001'
+        const { signature, recovery} = util.signTx(wallet.address, toAddr, amount, fee, nonce, wallet.privateKey)
+        logger.debug('-------------------------------')
+        logger.debug('sign the tx with private key')
+        logger.debug(signature, recovery)
+        const signedTx = {
+            amount,
+            fee,
+            from: wallet.address,
+            to: toAddr,
+            nonce,
+            signature,
+            recovery
+        }
+        logger.debug(signedTx)
+        logger.debug('Ready, sending....')
+        const ret = await outgoingTx(signedTx)
+        logger.debug('Result ==>')
+        logger.debug(ret)
+        if (!("txhash" in ret) || (typeof ret.txhash) !== "string") {
+            return console.error("Fail to transfer hycon.")
+        } else {
+            return logger.debug(`Sent successful: ${ret.txhash}`)
+        }
+    } catch (e) {
+        logger.error(e)
+    }
+}
+
+main().catch((e)=>{
+    logger.error(e)
+})
